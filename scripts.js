0916901ac62482017ab42b7ab396dfaf584bb6ab
victorpaulosg@gmail.com <victorpaulosg@gmail.com>
2022-08-07 13:47:41 -0300

    html puro, javascript pronto

---
+++ scripts.js
@@ -0,0 +1,49 @@
+var string;
+const vogais = ["a","e","i","o","u"];
+const caracteres = ["ai","enter","imes","ober","ufat"];
+var contador = 0;
+function separaString(string){
+    while(contador<string.length){
+        for(var i=0; i<5;i++){
+            for(;string[contador]==vogais[i];){
+                string[contador]=caracteres[i];
+                console.log(string);
+                contador++;
+                i=0;
+            }
+        }
+        contador++;
+    }
+    i=0;
+    contador=0;
+    return string;
+}
+function encripta(string){
+    string = string.toString();
+    string = string.replace(/,/g,"");
+    return string;
+}
+function descriptografar(string){
+    string = string.replace(/ai/g, "a");
+    string = string.replace(/enter/g, "e");
+    string = string.replace(/imes/g, "i");
+    string = string.replace(/ober/g, "o");
+    string = string.replace(/ufat/g, "u");
+    return string;
+}
+function botCodificar(){
+    resultado=[];
+    string = document.querySelector("input").value;
+    string = string.split("");
+    string = separaString(string);
+    string = encripta(string);
+    console.log(string);
+    document.getElementById("resultado").innerHTML=string;
+}
+function botDecode(){
+    resultado=[];
+    string = document.querySelector("input").value;
+    //string = string.split("");
+    string = descriptografar(string);
+    document.getElementById("resultado").innerHTML=string;
+}
